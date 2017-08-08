ca0359a0499e80ab7fd682c9c1c438c83372b5c5
abatten <abatten95@gmail.com>
2017-08-08 15:28:33 +1000

    Added constants file

---
+++ scripts/constants.py
@@ -0,0 +1,69 @@
+"""
+This file is a list of physical constants
+
+PREFIXES:
+
+X indicates the unit is in SI units.
+Y indicates the unit is in cgs units
+"""
+XYR = 31557600          # 1 year (s)
+XDAY = 86400            # 1 day (s)
+XHR = 3600              # 1 hour (s)
+
+XGRAM = 0.001           # Gram (kg)
+YKG = 1000              # Kilogram (g)
+
+XMSUN = 1.9886e30       # Solar Mass (kg)
+YMSUN = 1.9886e33       # Solar mass (g)
+XRSUN = 6.957e8         # Solar Radius (m)
+YRSUN = 6.957e11        # Solar Radius (cm)
+XLSUN = 3.839e26        # Solar Luminosity (W)
+YLSUN = 3.839e33        # Solar Luminosity (ergs/s)
+
+XMEARTH = 5.972e24      # Earth Mass (kg)
+YMEARTH = 5.972e27      # Earth Mass (g)
+XREARTH = 6.371e6       # Earth Radius (m)
+YREARTH = 6.371e8       # Earth Radius (cm)
+XGEARTH = 9.81          # Earth Gravitational Acceleration (m/s^2)
+
+XMJUP = 1.898e27        # Jupiter Mass (kg)
+YMJUP = 1.898e30        # Jupiter Mass (g)
+XRJUP = 7.1492e7        # Jupiter Radius (m)
+YRJUP = 7.1492e9        # Jupiter Radius (cm)
+
+XMELE = 9.109382e-31    # Electron mass (kg)
+YMELE = 9.109382e-28    # Electron mass (g)
+
+XMHYD = 1.6735325e-27   # Mass of Hydrogen Atom (kg)
+YMHYD = 1.6735325e-24   # Mass of Hydrogen Atom (g)
+
+XKM = 1.0e3             # 1 kilometer (m)
+YKM = 1.0e4             # 1 kilometer (cm)
+XCM = 0.01              # 1 centimeter (m)
+YM = 100                # 1 meter (cm)
+
+XLY = 9.461e15          # Light year (m)
+YLY = 9.461e18          # Light year (cm)
+
+XAU = 1.4960e11         # Astronomical Unit (m)
+YAU = 1.4960e13         # Astronomical Unit (cm)
+
+YJ = 1.0e7              # 1 Joule (ergs)
+XERG = 1.0e-7           # 1 erg (J)
+
+XG = 6.67408e-11        # Gravitational Constant (m^3/kg/s^2)
+YG = 6.67408e-8         # Gravitational constant (cm^3/g/s^2)
+
+XCLIGHT = 299792458     # Speed of light in a vaccum (m/s)
+YCLIGHT = 29979245800   # Speed of light in a vacuum (cm/s)
+
+XEV = 1.602176462e-19   # 1 electron volt (J)
+YEV = 1.602176462e-12   # 1 electron volt (ergs)
+
+XK = 1.380658e-23       # Boltzmann Constant (J/K)
+YK = 1.380658e-16       # Boltzmann Constant (erg/K)
+
+XNA = 6.0221e23         # Avagadros Number (mol^-1)
+
+XR = 8.3145             # Gas Constant (J/mol/K)
+YR = 8.3145e7           # Gas Constant (ergs/mol/K)
--- scripts/nbody.py
+++ scripts/nbody.py
@@ -1,27 +1,21 @@
 import tkinter as tk
 import numpy as np
-import time
-import os
-import sys
 import argparse
 
-######### CONSTANTS ##########
-R_sun = 6.957e8 # Solar Radius in meters
-M_sun = 2e30 # Solar Mass in kg
-year = 365.25 * 24 * 60 * 60 # 1 year in s
-grav_constant = 6.67e-11
-km = 10**3.0 # 1km in m
-################################################################################
+import constants as c
+
+
+###############################################################################
 class Particle:
-    def __init__(self, init_pos, init_vel, mass, radius, index, colour='#000000'):
+    def __init__(self, init_pos, init_vel, mass, rad, dex, colour='#000000'):
         """ Initialise the parameters of the particle """
 
-        self.position = init_pos
-        self.velocity = [y * km for y in init_vel]
-        self.mass = mass * M_sun
-        self.radius = radius * R_sun
+        self.pos = init_pos
+        self.vel = [y * c.XKM for y in init_vel]
+        self.mass = mass * c.XMSUN
+        self.radius = rad * c.XRSUN
         self.colour = colour
-        self.index = index
+        self.index = dex
 
         self.properties()
         self.draw()
@@ -30,16 +24,16 @@ class Particle:
         """ Print out the properties of the particle. """
 
         print("Particle:", self.index)
-        print("Position:", self.position)
-        print("Velocity:", self.velocity)
+        print("Position:", self.pos)
+        print("Velocity:", self.vel)
 
     def draw(self):
         """ Draw the particle at its position."""
 
-        frame.create_oval((self.position[0] * scale) - (self.radius * scale) - 2,
-                          (self.position[1] * scale) - (self.radius * scale) - 2,
-                          (self.position[0] * scale) + (self.radius * scale) - 2,
-                          (self.position[1] * scale) + (self.radius * scale) - 2,
+        frame.create_oval((self.pos[0] * scale) - (self.radius * scale) - 2,
+                          (self.pos[1] * scale) - (self.radius * scale) - 2,
+                          (self.pos[0] * scale) + (self.radius * scale) - 2,
+                          (self.pos[1] * scale) + (self.radius * scale) - 2,
                           tag="Particle" + str(self.index) + "Shape",
                           fill=self.colour, disableddash=True)
 
@@ -55,11 +49,11 @@ class Particle:
 
         """
 
-        x1 = self.position[0]
-        x2 = other.position[0]
+        x1 = self.pos[0]
+        x2 = other.pos[0]
 
-        y1 = self.position[1]
-        y2 = other.position[1]
+        y1 = self.pos[1]
+        y2 = other.pos[1]
 
         distance = np.sqrt((x2 - x1)**2.0 + (y2 - y1)**2.0)
 
@@ -72,42 +66,42 @@ class Particle:
 
         """
 
-        dx = self.velocity[0] * dt
-        dy = self.velocity[1] * dt
+        dx = self.vel[0] * dt
+        dy = self.vel[1] * dt
 
-        self.position[0] += dx
-        self.position[1] += dy
+        self.pos[0] += dx
+        self.pos[1] += dy
 
-        if cycle % (2) == 0 and tracks == True:
-            frame.create_oval(self.position[0] * scale - 2,
-                              self.position[1] * scale - 2,
-                              self.position[0] * scale + 2,
-                              self.position[1] * scale + 2,
+        if cycle % (2) == 0 and tracks:
+            frame.create_oval(self.pos[0] * scale - 2,
+                              self.pos[1] * scale - 2,
+                              self.pos[0] * scale + 2,
+                              self.pos[1] * scale + 2,
                               tag="Particle" + str(self.index) + "Track",
                               fill=self.colour, disableddash=True)
 
-        frame.move("Particle" + str(self.index) + "Shape", 
+        frame.move("Particle" + str(self.index) + "Shape",
                    dx * scale, dy * scale)
 
-################################################################################
+
 def gravitation(particles, dt):
     """
     Calculates the gravitational force on each particle and changes
-    the velocity of the particle appropriatly. 
+    the velocity of the particle appropriatly.
 
     """
     for p1 in particles:
         for p2 in particles:
-            if p1.index != p2.index: # No self gravity
-                
-                # Calculate the force on the particle                
-                Ftop = grav_constant * p1.mass * p2.mass
+            if p1.index != p2.index:  # No self gravity
+
+                # Calculate the force on the particle
+                Ftop = c.XG * p1.mass * p2.mass
                 Fbot = p1.distance_to(p2)**2.0
                 F = Ftop / Fbot
-                                
+
                 # Find direction of the force
-                deltaX = p2.position[0] - p1.position[0]
-                deltaY = p2.position[1] - p1.position[1]
+                deltaX = p2.pos[0] - p1.pos[0]
+                deltaY = p2.pos[1] - p1.pos[1]
                 theta = np.arctan2(deltaX, deltaY)
 
                 # Find the x,y components of the force
@@ -119,8 +113,9 @@ def gravitation(particles, dt):
                 ay = Fy / p1.mass
 
                 # Calculate the new velocity of the particle
-                p1.velocity[0] += (ax * dt)
-                p1.velocity[1] += (ay * dt)
+                p1.vel[0] += (ax * dt)
+                p1.vel[1] += (ay * dt)
+
 
 def draw_grid(frame, window_size, box_size, tick_num, tick_length):
     """ Draws the grid lines and values around the outside of the box."""
@@ -129,52 +124,53 @@ def draw_grid(frame, window_size, box_size, tick_num, tick_length):
     tick_jump = box_size / tick_num
 
     # Physical Distance between ticks on the screen
-    tick_spacing = window_size / tick_num
-
+    tick_space = window_size / tick_num
 
     # Draw the tick marks
     for i in range(tick_num):
-        frame.create_line(0, i * tick_spacing,
-                          tick_length, i * tick_spacing,
+        frame.create_line(0, i * tick_space,
+                          tick_length, i * tick_space,
                           tag="LeftTicks", fill="#000000")
 
-        frame.create_line(window_size - tick_length, i * tick_spacing,
-                          window_size, i * tick_spacing,
+        frame.create_line(window_size - tick_length, i * tick_space,
+                          window_size, i * tick_space,
                           tag="RightTicks", fill="#000000")
 
-        frame.create_line(i * tick_spacing, 0,
-                          i * tick_spacing, tick_length, 
+        frame.create_line(i * tick_space, 0,
+                          i * tick_space, tick_length,
                           tag="TopTicks", fill="#000000")
 
-        frame.create_line(i * tick_spacing, window_size - tick_length,
-                          i * tick_spacing, window_size,
+        frame.create_line(i * tick_space, window_size - tick_length,
+                          i * tick_space, window_size,
                           tag="BottomTicks", fill="#000000")
 
     # Annotate the tick marks with values
     for i in range(tick_num - 1):
         # Left Tick Labels
-        frame.create_text(tick_length + 5, (i + 1) * tick_spacing,
-                          text=str("{0:.2f}".format(((i + 1) * tick_jump)/ R_sun)) + " Rsun",
-                          anchor="w")
+        frame.create_text(tick_length + 5,
+                          (i + 1) * tick_space,
+                          text=str("{0:.2f}".format(
+                           (i + 1) * tick_jump / c.XRSUN)) + "Rsun", anchor="w")
         # Top Tick Labels
-        frame.create_text((i + 1) * tick_spacing, tick_length + 5, 
-                          text=str("{0:.2f}".format(((i + 1) * tick_jump)/ R_sun)) + " Rsun",
-                          anchor="c")
+        frame.create_text((i + 1) * tick_space, tick_length + 5,
+                          text=str("{0:.2f}".format(
+                           (i + 1) * tick_jump / c.XRSUN)) + "Rsun", anchor="c")
+
 
 def time_display(frame, time):
     time_box_width = 120
     time_box_height = 20
-    frame.create_rectangle(window_size - time_box_width - tick_length - 30, 
+    frame.create_rectangle(window_size - time_box_width - tick_length - 30,
                            window_size - tick_length - 30,
-                           window_size - tick_length - 30, 
-                           window_size - time_box_height - tick_length - 30, 
+                           window_size - tick_length - 30,
+                           window_size - time_box_height - tick_length - 30,
                            outline="white", fill="white")
 
-    frame.create_text(window_size - time_box_width - tick_length - 20, 
-                      window_size - tick_length - 40, 
+    frame.create_text(window_size - time_box_width - tick_length - 20,
+                      window_size - tick_length - 40,
                       text=str("{0:.5f}".format(time)) + " yr", anchor="w")
 
-################################################################################
+
 if __name__ == "__main__":
 
     # Add the arguments for the user
@@ -199,28 +195,28 @@ if __name__ == "__main__":
         window_size = 1000
 
     if args.boxsize:
-        box_size = args.boxsize * R_sun
+        box_size = args.boxsize * c.XRSUN
         scale = window_size/box_size
     else:
-        box_size = 1000 * R_sun
+        box_size = 1000 * c.XRSUN
         scale = window_size/box_size
 
     if args.timestep:
         time_step = args.timestep
     else:
-        time_step = 24 * 3600 # 0.1 day in seconds
+        time_step = c.XDAY  # 0.1 day in seconds
 
     if args.ticknum:
         tick_num = args.ticknum
     else:
-        tick_num = 10 
+        tick_num = 10
 
     if args.ticklength:
         tick_length = args.ticklength
     else:
-        tick_length = 20 
+        tick_length = 20
 
-    # Create window and frame 
+    # Create window and frame
     window = tk.Tk()
     window.title("N-Body Simulation")
     frame = tk.Canvas(window, height=str(window_size), width=str(window_size))
@@ -228,14 +224,17 @@ if __name__ == "__main__":
     frame.configure(background="white")
 
     draw_grid(frame, window_size, box_size, tick_num, tick_length)
-  
-    particle0 = Particle([box_size/2, box_size/2], [0,6.65], 9.9, 300, colour="#FF0000", index=0)
-    particle1 = Particle([box_size/2 - 1400 * R_sun, box_size/2], [0,-59.85], 1.1, 30, colour="#00FF00", index=1)
-    particle2 = Particle([box_size/2 - 500 * R_sun, box_size/2], [0,-36.79], 1.3, 30, colour="#0000FF", index=2)
+    particle0 = Particle([box_size/2, box_size/2],
+                         [0, -6.65], 9.9, 300, colour="#FF0000", dex=0)
 
-    #particles = [particle0, particle1]
+    particle1 = Particle([box_size/2 - 1400 * c.XRSUN, box_size/2],
+                         [0, 59.85], 1.1, 30, colour="#00FF00", dex=1)
+
+    particle2 = Particle([box_size/2 - 500 * c.XRSUN, box_size/2],
+                         [0, 36.79], 1.3, 30, colour="#0000FF", dex=2)
+
+    # particles = [particle0, particle1]
     particles = [particle0, particle1, particle2]
-
 
     max_cycles = 100000
     for cycle in range(max_cycles):
@@ -243,7 +242,7 @@ if __name__ == "__main__":
             gravitation(particles, time_step)
             particles[j].update_position(time_step, cycle, tracks=False)
 
-            current_time = cycle * time_step / year
+            current_time = cycle * time_step / c.XYR
             time_display(frame, current_time)
             frame.update()
 
