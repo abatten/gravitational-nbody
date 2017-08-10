431ad611ffc04d46bdec0fd69e27415317e6e8f1
abatten <abatten95@gmail.com>
2017-08-10 10:39:35 +1000

    Added Docstrings to functions

--- scripts/nbody2.py
+++ scripts/nbody2.py
@@ -33,13 +33,12 @@ class Particle:
         """
         pyg.draw.circle(win, self.col, (self.pos[0],self.pos[1]), self.rad , 0)
 
-        return
+        return()
 
 
-def draw_axes(win, win_size, box_size, tick_num, tick_len):
+def draw_axes(win, window_size, box_size, tick_num, tick_length):
     """
     Draws the grid lines and labels around the outside of the box.
-
     Parameters
     ----------
     win_size : int
@@ -57,25 +56,24 @@ def draw_axes(win, win_size, box_size, tick_num, tick_len):
     """
 
     tick_jump = box_size / tick_num  # Value difference between axis labels
-    tick_space = win_size / tick_num  # Physical distance bwtween axis labels
+    tick_space = window_size / tick_num  # Physical distance bwtween axis labels
     tick_colour = 0x000000  # Black
-    tick_thickness = 2
 
     # Draw the tick marks
     for i in range(tick_num):
 
         #  Left Ticks
         pyg.draw.line(win, tick_colour, (0, i * tick_space),
-                          (tick_len, i * tick_space), tick_thickness)
+                          (tick_length, i * tick_space), 2)
         #  Right Ticks
-        pyg.draw.line(win, tick_colour, (win_size - tick_len, i * tick_space),
-                          (win_size, i * tick_space), tick_thickness)
+        pyg.draw.line(win, tick_colour, (window_size - tick_length, i * tick_space),
+                          (window_size, i * tick_space), 2)
         #  Top Ticks
         pyg.draw.line(win, tick_colour, (i * tick_space, 0),
-                          (i * tick_space, tick_len), tick_thickness)
+                          (i * tick_space, tick_length), 2)
         # Bottom Ticks
-        pyg.draw.line(win, tick_colour, (i * tick_space, win_size - tick_len),
-                          (i * tick_space, win_size), tick_thickness)      
+        pyg.draw.line(win, tick_colour, (i * tick_space, window_size - tick_length),
+                          (i * tick_space, window_size), 2)      
 
     #  Font Type    
     tick_font = pyg.font.SysFont("monospace", 15)
@@ -85,20 +83,17 @@ def draw_axes(win, win_size, box_size, tick_num, tick_len):
         label = tick_font.render(str("{0:.2f}".format((i + 1) * tick_jump / C.XRSUN)) + "Rsun", 2, (0,0,0))
 
         #  Display Left Ticks
-        win.blit(label, (tick_len + 5, (i+1) * tick_space - label.get_height() / 2.0 ))
+        win.blit(label, (tick_length + 5, (i+1) * tick_space - label.get_height() / 2.0 ))
 
         #  Display Top Ticks
-        win.blit(label, ((i+1) * tick_space - label.get_width() / 2.0, tick_len + 5))
+        win.blit(label, ((i+1) * tick_space - label.get_width() / 2.0, tick_length + 5))
 
-        return
 
-def time_display(win, time, win_size, tick_len):
+def time_display(win, time, window_size, tick_length):
     """
     Create Time Box in lower right hand corner.
-
     Creates a box to display the current time in the simulation.
     The time is specified to 4 decimal places.
-
     Parameters
     ----------
     win_size : int
@@ -117,26 +112,23 @@ def time_display(win, time, win_size, tick_len):
     text_colour = (0,0,0)
 
     #  rect(x,y, width, height, fill=True)
-    pyg.draw.rect(win, rect_colour,(win_size - time_box_width - tick_len - 30,
-                                    win_size - time_box_height - tick_len - 30,
+    pyg.draw.rect(win, rect_colour,(window_size - time_box_width - tick_length - 30,
+                                    window_size - time_box_height - tick_length - 30,
                                     time_box_width,
                                     time_box_height), 0)
 
     timer_font = pyg.font.SysFont("monospace", 15)
     timer = timer_font.render(str("{0:.5f}".format(time)) + " yr", 2, text_colour)
-    win.blit(timer, (win_size - time_box_width - tick_len - 20,
-                     win_size - tick_len - 40 - timer.get_height() / 2.0) )
+    win.blit(timer, (window_size - time_box_width - tick_length - 20,
+                     window_size - tick_length - 40 - timer.get_height() / 2.0) )
 
-    return
 
-def initialise_display(win_size, box_size, tick_num, tick_len, time=0):
+def initialise_display(window_size, box_size, tick_num, tick_length, time):
     """
     Initialise the window that everything will be displayed in.
-
     Creates a screen using pygame. Initialises the background and 
     draws axes along each edge and displays the current time in the 
     bottom right hand corner.
-
     Parameters
     ----------
     win_size : int
@@ -149,78 +141,33 @@ def initialise_display(win_size, box_size, tick_num, tick_len, time=0):
         The length of with tick mark in pixels.
     time : float
         The initial time to print in the bottom right hand corner.
-
     Returns
     -------
     win : pygame.display
         The window that everything will be displayed in.
-
     """
 
     pyg.init()
-    win = pyg.display.set_mode((win_size,win_size))
+    win = pyg.display.set_mode((window_size,window_size))
     win.fill((255,255,255))
     pyg.display.set_caption("N-Body 3D gravitational Simulator")
 
-    #  Add axes
-    draw_axes(win, win_size, box_size, tick_num, tick_len)
-    #  Display the initial time
-    time_display(win, time, win_size, tick_len)
-    
-    #  Refresh the display
+    draw_axes(win, window_size, box_size, tick_num, tick_length)
+    time_display(win, time, window_size, tick_length)
     pyg.display.flip() 
 
     return win
 
 
 def main():
-    # Add the arguments for the user
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--boxsize", help="The dimentions of the box in solar \
-                         radii. Default: 1000", type=int)
-    parser.add_argument("--timestep", help="The length of the time step \
-                         between calculations. Default: 1 day", type=float)
-    parser.add_argument("--winsize", help="The height and width of the \
-                         window in pixels. Default: 1000", type=int)
-    parser.add_argument("--ticknum", help="The number of ticks on each side \
-                         of the window. Default: 10", type=int)
-    parser.add_argument("--ticklen", help="The length of each tick on the \
-                         side of the window in pixels. Default: 20", type=int)
 
-    args = parser.parse_args()
-
-    # Setup defaults and read arguments
-    if args.winsize:
-        win_size = args.winsize
-    else:
-        win_size = 1000
-
-    if args.boxsize:
-        box_size = args.boxsize * C.XRSUN
-        scale = win_size/box_size
-    else:
-        box_size = 1000 * C.XRSUN
-        scale = win_size/box_size
-
-    if args.timestep:
-        time_step = args.timestep
-    else:
-        time_step = C.XDAY  # 0.1 day in seconds
-
-    if args.ticknum:
-        tick_num = args.ticknum
-    else:
-        tick_num = 10
-
-    if args.ticklen:
-        tick_len = args.ticklen
-    else:
-        tick_len = 20
-
-
+    window_size = 1000
+    box_size = 1000 * C.XRSUN
+    tick_num = 10
+    tick_length = 20
     time = 0
 
-    win = initialise_display(win_size, box_size, tick_num, tick_len)
+    win = initialise_display(window_size, box_size, tick_num, tick_length, time)
 
     Plist = [Particle(win, [500, 200, 300], [0, 0, 0], 10, 10, 0)]
 
@@ -228,6 +175,28 @@ def main():
     while running:
         time += 1
         pyg.display.flip()  # Refresh Display
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         for event in pyg.event.get():
             if event.type == pyg.QUIT:
